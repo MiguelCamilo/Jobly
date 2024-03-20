1c4b08df806065c44df2bd6fd043cc7682a79805
Miguel Camilo <miguelcamilo48@gmail.com>
2024-03-19 22:14:37 -0400

    feat: added job-type constants

--- src/components/job-filter-sidebar.tsx
+++ src/components/job-filter-sidebar.tsx
@@ -1,26 +1,24 @@
 import filterJobs from "../../actions/filter-jobs";
-import findAllJobLocations from '../../actions/find-all-locations';
+import findAllJobLocations from "../../actions/find-all-locations";
+import { JOB_TYPES } from "@/lib/constants/job-types";
 
+import { Checkbox } from "@/components/ui/checkbox";
 import { Label } from "@/components/ui/label";
+import { Button } from "./ui/button";
 import { Input } from "@/components/ui/input";
-import Select from '@/components/ui/select';
+import Select from "@/components/ui/select";
 
 const JobFilterSidebar = async () => {
+  const jobLocations = (await findAllJobLocations().then((locations) =>
+    // takes the location out of each object then
+    // turns it into an array of strings and by using
+    // .filter(Boolean) it removes any null values
+    locations.map(({ location }) => location).filter(Boolean),
+  )) as string[]; // using type assertion to tell typescript that this is a string array
 
-  const jobLocations = ( await findAllJobLocations()
-    .then((locations) =>
-      // takes the location out of each object then 
-      // turns it into an array of strings and by using 
-      // .filter(Boolean) it removes any null values
-      locations.map(({ location }) => location).filter(Boolean)
-    )) as string[]; // using type assertion to tell typescript that this is a string array
-  
   return (
     // h-fit onlys makes the the height fit the content
-    <aside className="sticky top-0 h-fit rounded-lg bg-background md:w-[260px] p-4">
-      {/* using the action prop allows to submit a server action which automatically
-            will be a POST endpoint
-        */}
+    <aside className="sticky top-0 h-fit rounded-lg bg-background p-4 md:w-[260px]">
       <form action={filterJobs}>
         <div className="space-y-4">
           <div className="flex flex-col gap-2">
@@ -32,20 +30,46 @@ const JobFilterSidebar = async () => {
             />
           </div>
 
-          <div className='flex flex-col gap-2'>
-            <Label htmlFor="location">Location</Label>
-            <Select
-              id='location'
-              name='location'
-              defaultValue=""
-            >
-              <option
-                value=""
-              >
-                All Locations
-              </option>
-            </Select>        
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="type">Type</Label>
+            <Select id="type" name="type" defaultValue="">
+              <option value="">All Types</option>
+              {JOB_TYPES.map((types) => (
+                <option key={types} value={types}>
+                  {types}
+                </option>
+              ))}
+            </Select>
           </div>
+
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="location">Location</Label>
+            <Select id="location" name="location" defaultValue="">
+              <option value="">All Locations</option>
+              {jobLocations.map((location) => (
+                <option key={location} value={location}>
+                  {location}
+                </option>
+              ))}
+            </Select>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <Checkbox
+              id="remote"
+              name="remote"
+              className="scale-125 accent-black"
+            />
+            <Label htmlFor="remote">Remote Jobs</Label>
+          </div>
+        <Button 
+          type="submit"
+          variant={"default"}
+          size={"icon"}
+          className='w-full'
+        >
+          Apply Filters
+        </Button>
         </div>
       </form>
     </aside>
---
+++ src/lib/constants/job-types.ts
@@ -0,0 +1,15 @@
+
+export const JOB_TYPES = [
+    "Full Time",
+    "Part Time",
+    "Contract",
+    "Temporary",
+    "Internship",
+    "Volunteer",
+]
+
+export const LOCATION_TYPES = [
+    "Remote",
+    "Onsite",
+    "Hybrid",
+]
