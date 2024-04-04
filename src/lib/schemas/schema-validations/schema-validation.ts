7ab9adae21da05f324ceebcf943f32cf7fa99380
Miguel Camilo <miguelcamilo48@gmail.com>
2024-04-04 08:38:58 -0400

    update: creating job creation schema and validations

---
+++ src/lib/schemas/schema-validations/schema-validation.ts
@@ -0,0 +1,16 @@
+import * as z from 'zod'
+
+export const requiredValidation = z
+  .string()
+  .min(1, "Minimum of one character required.");
+
+export const numericValidation  = requiredValidation.regex(/^\d+$/, "Value must me a number.")
+
+export const companyLogoUrlValidation = 
+  z.custom<File | undefined>()
+  .refine((file) => {
+    !file || (file instanceof File && file.type.startsWith("image/"));
+  }, "Invalid file type. Please upload an image file.")
+  .refine((file) => {
+    return !file || file.size < 1024 * 1024 * 2; // max size of 2MB
+  }, "File must be less than 2MB.");
--- src/lib/schemas/validation.ts
+++ src/lib/schemas/validation.ts
@@ -1,14 +1,35 @@
 import * as z from "zod";
 
-const requiredValidation = (minimumTextLenght: number, errorMessage: string) => {
-    return z.string().min(minimumTextLenght, { message: errorMessage });
-}  
+import { JOB_TYPES } from "../constants/job-types";
+
+import {
+  requiredValidation,
+  numericValidation,
+  companyLogoUrlValidation,
+} from "./schema-validations/schema-validation";
+
+const ApplicationSchema = z.object({
+    // .or(z.literal("")) is used to allow an empty string but still allowing .email() to validate
+    applicationEmail: z.string().max(100).email().optional().or(z.literal("")),
+    applicationUrl: z.string().max(100).email().optional().or(z.literal("")),
+}).refine(data => data.applicationEmail || data.applicationUrl, {
+    message: "Either an email or a URL is required.",
+    path: ["applicationEmail"], // if this error is thrown, it will be shown on the applicationEmail field
+});
 
 export const CreateJobSchema = z.object({
-  title: z.string(requiredValidation(5, "Title requires a minimum of 5 characters")),
-  type: z.string(),
-  locationType: z.string(),
-});
+  title: requiredValidation.max(100),
+  type: requiredValidation.refine(
+    (types) => JOB_TYPES.includes(types),
+    "Invalid Job Type.",
+  ),
+  companyName: requiredValidation.max(100),
+  companyLogoUrl: companyLogoUrlValidation,
+  description: z.string().max(5000).optional(),
+  salary: numericValidation.max(9,
+    "Salary value can't be longer than 9 digits.",
+  ),
+}).and(ApplicationSchema); // merges another schema but allows for seperate validation errors to be thrown
 
 export const JobFilterSchema = z.object({
   query: z.string().optional(),
